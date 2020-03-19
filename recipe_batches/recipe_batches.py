6e2a0f10156b1c4a3b5fd9bf443dc33fb2396428
DAVID HANG <davidnhang@gmail.com>
2020-03-19 10:30:30 -0700

    adding recipe

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,18 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  increaseList = []
+  check = [AskFor for AskFor in recipe.keys()]
+  Have = [have for have in ingredients.keys()]
+  for (Recipekey,RecipeValue), (IngredientKey2,IngredientValue2) in zip(recipe.items(), ingredients.items()):
+    if(IngredientValue2 <  RecipeValue ):
+      return 0
+    elif (len(check) != len(Have)):
+      return 0
+    else:
+      increaseList.append(IngredientValue2//RecipeValue)
+  minasdf = min(increaseList)
+  return minasdf
 
 
 if __name__ == '__main__':
@@ -12,3 +23,4 @@ if __name__ == '__main__':
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+  print(recipe_batches({'milk': 100, 'butter': 50, 'cheese': 10}, {'milk': 198, 'butter': 52, 'cheese': 10}))
